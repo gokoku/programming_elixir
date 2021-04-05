d252d00aabe123ce85c94273effbbd0f3f78097d
george <george.6809@gmail.com>
2021-04-05 22:25:27 +0900

    練習問題 ListsAndRecursion-5 splitできた

--- chapter_10/list_and_recursion_5.exs
+++ chapter_10/list_and_recursion_5.exs
@@ -1,4 +1,7 @@
 defmodule MyEnum do
+  #
+  # all?
+  #
   def all?([], _), do: true
   def all?([head | tail], cond) do
     if cond.(head) do
@@ -8,12 +11,18 @@ defmodule MyEnum do
     end
   end
 
+  #
+  # each
+  #
   def each([], _), do: :ok
   def each([head | tail], func) do
     func.(head)
     each(tail, func)
   end
 
+  #
+  # filter
+  #
   def filter([], _), do: []
   def filter([head | tail], cond) do
     if cond.(head) do
@@ -22,4 +31,18 @@ defmodule MyEnum do
       filter(tail, cond)
     end
   end
+
+  #
+  # split
+  #
+  def split(list, n) when n < length(list) do
+    { _pre(list, 0, n), _rest(list, n, length(list)) }
+  end
+  def split(list, _), do: { list, [] }
+
+  defp _pre( _, y, y), do: []
+  defp _pre([head | tail], x, y), do: [ head | _pre(tail, x+1, y) ]
+  defp _rest(list, 0, n) when n > 0, do: list
+  defp _rest([_head | tail], n, m), do: _rest(tail, n-1, m-1)
+
 end
